a70f2eb5b3c818201f87fa3d5b905e1fee663fa5
unknown <yanspfc97@gmail.com>
2021-08-12 21:48:33 -0400

    criando repo rick and morty

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+        <div id="title" class="text-center">
+            <h1>Rick and Morty Library</h1>
+        </div>
+        <Search/>
+        <div id="main" class="main">
+          <div class="container max-height-1000">
+            <div class="row content-center">
+              <div class="col-md-6 card-infos " id="card-character" v-for="character in characters" v-bind:key="character">
+                      <div>
+                        <label for=""></label>
+                      </div>
+                      <div>
+                        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" style="width: 100px; height: 100px; border-radius: 8px;">
+
+                      </div>
+                </div>
+            </div>
+          </div>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+import Search from './components/Search.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "./styles/styles.css"
+export default {
+  name: 'App',
+  components: {
+    Search
+  }, 
+
+  created: function(){
+    axios.get('https://rickandmortyapi.com/api/character/').then(response => (console.log(response.data)));
+    
+  },
+  data: function (){
+    return{
+      characters: [],
+    }
+  }
+  // methods:{
+  //   listCharacters(id){
+  //     this.characterId = id;
+  //     console.log(this.characterId)
+  //     axios.get(`https://rickandmortyapi.com/api/character/${this.characterId}`)
+  //   }
+  // }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="buscas" class="col-sm text-center">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+                <div>
+                  <h3>Buscar por personagem</h3>
+                  <input class="entrada col-12" type="text">
+                </div>
+               </div>
+                <div class="col-sm">
+                  <div>                
+                    <h3>Buscar por episódio</h3>
+                    <input class="entrada col-12" type="text" >
+                 </div>
+                </div>
+            </div>  
+          </div>
+        </div>
+</template>
+<script>
+
+export default {
+  name: 'Search'
+}
+</script>
