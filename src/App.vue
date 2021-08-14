08151e3459b06d3d65a7c394a5d5c3c94486ad5e
José Yan <yanspfc97@gmail.com>
2021-08-13 22:50:04 -0400

    comecando a mostrar as informações dinamicamente na tela

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
             <h1>Rick and Morty Library</h1>
         </div>
         <Search/>
-        <div>
+        <div v-for="chars in charData">
+          {{ chars.name }}
           <div>
-
+            <img :src="chars.image" alt="" style="heigth: 50px; width: 50px;">
           </div>
           <div>
             
@@ -28,18 +29,32 @@ export default {
     return{
       page: 1,
       pageSize: null,
+      charData: null,
       characters: 'https://rickandmortyapi.com/api/character',
     }
   },
   methods: {
     getCharacterList() {
       const axios = require('axios')
-      axios.get(`${ this.characters }/?pages=${ this.page }`).then(response => {
+      axios.get(`${ this.characters }/?page=${ this.page }`).then(response => {
         console.log('response is: ', response.data.results)
         this.pageSize = response.data.results.length
+        this.charData = response.data.results
+        console.log(this.charData[3].name)        
         console.log('o tamanho da pagina é de: ', this.pageSize)
       })
       console.log('Terminou a função')
+    },
+    nextPage() {
+      if(this.page < 34){
+        this.page++
+      }
+      this.getCharacterList(this.page)
+    },
+    previousPage() {
+      if(this.page > 0){
+        this.page--
+      }
     }
   },
   created: function() {
